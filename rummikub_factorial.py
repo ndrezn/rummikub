fac68fd7fa67ab200d81a6678c142ea9ac7d263a
Nathan Drezner <nathan@drezner.xyz>
2022-12-12 00:02:38 -0500

    Works for any size sets, with horrifying performance

--- rummikub_factorial.py
+++ rummikub_factorial.py
@@ -10,6 +10,9 @@ def is_valid(possible_run):
     colors = [i[1] for i in possible_run]
     values = [i[0] for i in possible_run]
 
+    if len(possible_run) < 3:
+        return False
+
     # Identity set (different colors, same values)
     if len(set(colors)) == len(possible_run) and len(set(values)) == 1:
         return True
@@ -25,42 +28,52 @@ def is_valid(possible_run):
     return False
 
 
-def get_runs(tile_orders, sets, size):
+def get_runs(tile_orders, sets, unused_tiles):
+    if not unused_tiles:
+        return sets, True
     if not tile_orders:
-        return sets
+        return sets, False
 
     for i in range(0, len(tile_orders)):
         order = tile_orders[i]
 
         # Size counter doesn't work because our end contition will not continue
-        for i in range(3, len(board) + 1):
+        for i in range(0, len(board) + 1):
             if is_valid(order[0:i]):
                 new_sets = sets + [order[0:i]]
-                tiles = order[i:]
-                new_orders = create_permutations(tiles)
-                if i == len(order):
-                    return new_sets
+
+                new_tiles = order[i:]
+                new_orders = create_permutations(new_tiles)
+                new_unused = [j for j in unused_tiles if j not in order[0:i]]
 
                 # Recursive step... head down this tree and see if
                 # it is a valid solution.
-                return get_runs(new_orders, new_sets, size)
+                sol_sets, is_solution = get_runs(new_orders, new_sets, new_unused)
 
-    return sets
+                if is_solution:
+                    return sol_sets, True
+
+    return sets, False
 
 
 board = [
     (3, "r"),
-    (3, "r"),
     (4, "r"),
     (5, "r"),
+    (6, "r"),
+    (3, "r"),
     (3, "y"),
+    (1, "w"),
     (3, "w"),
     (1, "y"),
     (1, "r"),
     (1, "b"),
 ]
 
+
+print(board)
+
 tile_orders = create_permutations(board)
-solution = get_runs(tile_orders, [], len(board))
+solution = get_runs(tile_orders, [], board)
 
 print(solution)
